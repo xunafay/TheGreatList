e3c431cb0c6f911101079960ff1e6c1fffd28d0a
xunafay <me@xunafay.io>
2022-12-13 11:34:58 +0100

    scaffold

---
+++ TheGreatList/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TheGreatList;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TheGreatList/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TheGreatList;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
